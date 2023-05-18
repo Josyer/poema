c7313d7caba234a8f9d6c52721d9a2deab9c03a3
Josyer <126198276+Josyer@users.noreply.github.com>
2023-05-18 12:47:03 -0600

    Add files via upload

---
+++ poema.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <pre>
+        <?php
+        echo "Arde en tus ojos un misterio, virgen
+            esquiva y compañera.
+            No sé si es odio o es amor la lumbre
+            inagotable de tu aliaba negra.
+
+            Conmigo irás mientras proyecte sombra
+            mi cuerpo y quede a mi sandalia arena.
+            -¿Eres la sed o el agua en mi camino?-
+            Dime, virgen esquiva y compañera.";
+        ?>
+    </pre>
+</body>
+</html>
